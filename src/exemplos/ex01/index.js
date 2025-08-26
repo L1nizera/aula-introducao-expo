ff3788f9046b2ff11977f6079dd95f30e945ed8c
Rhyan <l1nizerag0d@gmail.com>
2025-08-25 22:12:46 -0300

    inicial

---
+++ src/exemplos/ex01/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'; 
+
+import styles from './styles';
+
+export default function Exemplo01() {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Exemplo 1</Text>
+            <Text>Introdução React Native com Expo</Text>
+        </View>
+    );
+}
+
